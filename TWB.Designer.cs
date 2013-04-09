cb8f431087045984d60fabb50851150940d6ffeb
Jay Das <jay11421@gmail.com>
2013-04-09 09:31:13 -0700

    Right files!

---
+++ TWB.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace TextWebBrowser
+{
+    partial class TWB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.richTextBoxBody = new System.Windows.Forms.RichTextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(837, 7);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(45, 23);
+            this.buttonBack.TabIndex = 7;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // richTextBoxBody
+            // 
+            this.richTextBoxBody.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxBody.Location = new System.Drawing.Point(10, 36);
+            this.richTextBoxBody.Name = "richTextBoxBody";
+            this.richTextBoxBody.Size = new System.Drawing.Size(872, 529);
+            this.richTextBoxBody.TabIndex = 6;
+            this.richTextBoxBody.Text = "";
+            this.richTextBoxBody.TextChanged += new System.EventHandler(this.richTextBoxBody_TextChanged);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(788, 7);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(43, 23);
+            this.buttonGo.TabIndex = 5;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUrl.Location = new System.Drawing.Point(10, 7);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(772, 23);
+            this.textBoxUrl.TabIndex = 4;
+            this.textBoxUrl.Text = "http://2das.net/jay";
+            this.textBoxUrl.TextChanged += new System.EventHandler(this.textBoxUrl_TextChanged);
+            this.textBoxUrl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxUrl_KeyDown);
+            // 
+            // TWB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 573);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.richTextBoxBody);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.textBoxUrl);
+            this.Name = "TWB";
+            this.Text = "TWB";
+            this.Load += new System.EventHandler(this.TWB_Load);
+            this.Resize += new System.EventHandler(this.TWB_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.RichTextBox richTextBoxBody;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textBoxUrl;
+    }
+}
---
+++ TWB.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace TextWebBrowser
+{
+    public partial class TWB : Form
+    {
+        public TWB()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            makeRequest();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUrl_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBoxBody_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TWB_Load(object sender, EventArgs e)
+        {
+            resize();
+        }
+
+        void makeRequest()
+        {
+            string url = textBoxUrl.Text;
+            processHTTP ph = new processHTTP(url);
+            string responseText = ph.ResponseText;
+
+            //Remove Script
+            int x = responseText.IndexOf("<body");
+            int y = responseText.IndexOf("</body>") - x + 7;
+            responseText = responseText.Substring(x, y);
+            responseText = StripTagsCharArray(responseText);
+            responseText = responseText.Replace("	", "");
+            responseText = responseText.Replace("&nbsp;", "");
+            responseText = responseText.Replace("&#39;", "'");
+            responseText = responseText.Replace("&#160;", " ");
+            responseText = responseText.Replace("â€“", "-");
+
+
+            responseText = Regex.Replace(responseText, @"[\r\n]+", "\n");
+
+            richTextBoxBody.Text = responseText;
+        }
+
+        public static string StripTagsCharArray(string source)
+        {
+            char[] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                char let = source[i];
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+            return new string(array, 0, arrayIndex);
+        }
+        void resize()
+        {
+            richTextBoxBody.Width = this.Width - 50;
+            richTextBoxBody.Height = this.Height - 100;
+
+            richTextBoxBody.Width = this.Width - 10;
+            richTextBoxBody.Height = this.Height - 68;
+            richTextBoxBody.Location = new Point(1, 40);
+
+            buttonBack.Left = this.Width - 100;
+            buttonGo.Left = this.Width - 150;
+            textBoxUrl.Width = this.Width - 180;
+        }
+
+        private void TWB_Resize(object sender, EventArgs e)
+        {
+            resize();
+        }
+
+        private void textBoxUrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                makeRequest();
+            }
+        }
+        /*private void buttonGo_Click(object sender, EventArgs e)
+        {
+
+            gogogo();
+        }
+        void gogogo()
+        {
+            string url = textBoxUrl.Text;
+            processHTTP ph = new processHTTP(url);
+            string responseText = ph.ResponseText;
+
+            //Remove Script
+            int x = responseText.IndexOf("<body");
+            int y = responseText.IndexOf("</body>") - x + 7;
+            responseText = responseText.Substring(x, y);
+            responseText = StripTagsCharArray(responseText);
+            responseText = responseText.Replace("	", "");
+            responseText = responseText.Replace("&nbsp;", "");
+            responseText = responseText.Replace("&#39;", "'");
+            responseText = responseText.Replace("&#160;", " ");
+            responseText = responseText.Replace("â€“", "-");
+            
+
+            responseText = Regex.Replace(responseText, @"[\r\n]+", "\n");
+
+            richTextBoxBody.Text = responseText;
+        }
+
+        public static string StripTagsCharArray(string source)
+        {
+            char[] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                char let = source[i];
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+            return new string(array, 0, arrayIndex);
+        }
+
+        private void TWB_Load(object sender, EventArgs e)
+        {
+            resize();
+        }
+
+        private void TWB_Resize(object sender, EventArgs e)
+        {
+            resize();
+            
+        }
+        void resize()
+        {
+            richTextBoxBody.Width = this.Width - 50;
+            richTextBoxBody.Height = this.Height - 100;
+
+            richTextBoxBody.Width = this.Width - 10;
+            richTextBoxBody.Height = this.Height - 68;
+            richTextBoxBody.Location = new Point(1, 40);
+
+            buttonBack.Left = this.Width - 100;
+            buttonGo.Left = this.Width - 150;
+            textBoxUrl.Width = this.Width - 180;
+        }
+
+        private void textBoxUrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                gogogo();
+            }
+        }*/
+    }
+}
